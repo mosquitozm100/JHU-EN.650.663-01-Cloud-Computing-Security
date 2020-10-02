6fcd8ffcce899a28b80b5eb68348c92a9e2b4482
Mou Zhang <mosquitozm100@gmail.com>
2020-10-02 04:00:16 -0400

    lab2 all function finished

--- lab2/lab2/main.py
+++ lab2/lab2/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, json, jsonify, render_template, request, url_for, redirect, session, escape
+from flask import Flask, json, jsonify, render_template, request, url_for, redirect, session, escape, make_response, flash
 from google.cloud import datastore
 import os
+import bcrypt
+import datetime
+from datetime import timedelta, timezone
+from base64 import b64encode
+import uuid
 from werkzeug.debug import DebuggedApplication
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -16,16 +21,13 @@ if app.debug:
 
 DS = datastore.Client()  # The client to connect with Google cloud storage
 EVENT = 'Event'  # Name of the event table, can be anything you like.
+USERS = 'Users'
+SESSION = 'Session'
 ROOT = DS.key('Entities', 'root')  # Name of root key, can be anything.
-
-# Distinguish between data for local server and cloud server
-if os.getenv('GAE_ENV', '').startswith('standard'):
-    ROOT = DS.key('Entities', 'root')
-else:
-    ROOT = DS.key('Entities', 'dev')
+USER_KEY = DS.key('Entities', 'root')
 
 
-def encrypt_pswd(pswdStr, hash):
+def encrypt_pswd(pswdStr, hash=None):
     if hash == None:
         hash = bcrypt.gensalt()
     return bcrypt.hashpw(pswdStr, hash)
@@ -41,9 +43,18 @@ def put_event(name, dateStr):
     Returns:
         null
     '''
-    entity = datastore.Entity(key=DS.key(EVENT, parent=ROOT))
+    # print("lalal")
+    # print(USER_KEY)
+    entity = datastore.Entity(key=DS.key(EVENT, parent=USER_KEY))
     entity.update({'name': name, 'date': dateStr})
     DS.put(entity)
+
+
+def put_user(username, password):
+    user_key = DS.key('Users', username)
+    user = datastore.Entity(key=DS.key(USERS, parent=user_key))
+    user.update({'username': username, 'password': password})
+    DS.put(user)
 
 
 def request_parse(req_data):
@@ -61,26 +72,55 @@ def request_parse(req_data):
     return data
 
 
-def createSession(username):
-    session.create(username)
-    return 'yes'
+def create_session(username):
+    session_key = DS.key('Session', username)
+    session = datastore.Entity(key=DS.key(SESSION, parent=session_key))
+    random_secret_token = b64encode(os.urandom(64)).decode()
+
+    expire_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+
+    session.update({'token': random_secret_token, 'expire_time': expire_time})
+    DS.put(session)
+
+    resp = make_response(app.send_static_file('index.html'))
+    resp.set_cookie('user', username,
+                    max_age=60*60, expires=expire_time)
+    resp.set_cookie('token', random_secret_token,
+                    max_age=60*60, expires=expire_time)
+    return resp
 
 
-@app.route('/')
-@app.route('/index.html')
+def migrate_data(username):
+    print(USER_KEY)
+    for val in DS.query(kind=EVENT, ancestor=ROOT).fetch():
+        deleteKey = DS.key(EVENT, val.id, parent=ROOT)
+        DS.delete(deleteKey)
+        put_event(val['name'], val['date'])
+
+
+@ app.route('/')
+@ app.route('/index.html')
 def root():
     '''
     Direct these two request to the index.html page.
     '''
-    print(session)
-    if 'username' in session:
-        return app.send_static_file('index.html')
+    username = request.cookies.get('user')
+    token = request.cookies.get('token')
+    if token == None:
+        return redirect(url_for('login'))
+    session_key = DS.key('Session', username)
+    sessions = DS.query(kind=SESSION, ancestor=session_key).fetch()
+    for session in sessions:
+        if session['token'] == token:
+            if session['expire_time'] > datetime.datetime.now(timezone.utc):
+                return app.send_static_file('index.html')
+            else:
+                return redirect(url_for('logout'))
     else:
-        login_url = url_for('login')
-        return redirect(login_url)
+        return redirect(url_for('login'))
 
 
-@app.route('/events', methods=['GET'])
+@ app.route('/events', methods=['GET'])
 def events():
     '''
     Get all events stored in the google cloud database.
@@ -88,7 +128,7 @@ def events():
         id, name and date for all events in json
     '''
     events = []
-    for val in DS.query(kind=EVENT, ancestor=ROOT).fetch():
+    for val in DS.query(kind=EVENT, ancestor=USER_KEY).fetch():
         val['id'] = val.id
         events.append(val)
     jsonEvents = {}
@@ -96,7 +136,7 @@ def events():
     return jsonify(jsonEvents)
 
 
-@app.route('/event', methods=['POST'])
+@ app.route('/event', methods=['POST'])
 def event():
     '''
     Add a new event into google cloud storage datebase.
@@ -110,7 +150,7 @@ def event():
     return 'Create successfully'
 
 
-@app.route('/event/<int:event_id>', methods=['DELETE'])
+@ app.route('/event/<int:event_id>', methods=['DELETE'])
 def delete(event_id):
     '''
     Delete the event with input event_id from google cloud storage
@@ -120,7 +160,7 @@ def delete(event_id):
         Delete Success - 'Delete successfully.'
         Delete Fails - Error! Event not found!'
     '''
-    deleteKey = DS.key(EVENT, event_id, parent=ROOT)
+    deleteKey = DS.key(EVENT, event_id, parent=USER_KEY)
     try:
         event = DS.get(deleteKey)
         DS.delete(event.key)
@@ -130,40 +170,61 @@ def delete(event_id):
         return 'Delete successfully.'
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@ app.route('/login', methods=['GET', 'POST'])
 def login():
-    print(session)
     if request.method == 'GET':
         return app.send_static_file('login.html')
     elif request.method == 'POST':
-        print(request)
         username = request.form['username']
         password = request.form['password'].encode()
-        print(username)
-        print(password)
         user_key = DS.key('Users', username)
         users = DS.query(kind='Users', ancestor=user_key).fetch()
         for user in list(users):
-            print(user)
             if user['username'] == username and user['password'] == encrypt_pswd(password, user['password']):
-                createSession(username)
-                return app.send_static_file('index.html')
-            else:
-                return 'Error!'
+                global USER_KEY
+                USER_KEY = DS.key('Entities', username)
+                return create_session(username)
+        flash('Username and password not found!')
         return redirect(url_for('root'))
 
 
-@app.route('/signup')
+@ app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return app.send_static_file('signup.html')
+    if request.method == 'GET':
+        return app.send_static_file('signup.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password'].encode()
+        global USER_KEY
+        USER_KEY = DS.key('Entities', username)
+        put_user(username, encrypt_pswd(password))
+        migrate_data(username)
+        return create_session(username)
 
 
-@app.route('/logout')
+@ app.route('/logout')
 def logout():
     # remove the username from the session if it is there
-    print(session)
-    session.pop('username', None)
-    return redirect(url_for('root'))
+    username = request.cookies.get('user')
+    token = request.cookies.get('token')
+    if username == None:
+        return redirect(url_for('root'))
+    session_key = DS.key('Session', username)
+    sessions = DS.query(kind=SESSION, ancestor=session_key).fetch()
+    for session in sessions:
+        if session['token'] == token:
+            DS.delete(session.key)
+
+    expired_time = datetime.datetime.now() - datetime.timedelta(hours=1)
+    resp = make_response(redirect(url_for('root')))
+    resp.set_cookie('user', '',
+                    max_age=0, expires=expired_time)
+    resp.set_cookie('token', '',
+                    max_age=0, expires=expired_time)
+    flash('You have signed out!')
+    global USER_KEY
+    USER_KEY = DS.key('Entities', 'root')
+    return resp
 
 
 if __name__ == '__main__':
